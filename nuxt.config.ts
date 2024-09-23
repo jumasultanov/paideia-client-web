db2442dc70dacde7401e52a1cebcf92c16393c1a
jumasultanov <jumasultanov@rambler.ru>
2024-09-23 23:46:14 +0500

    install nessesary plugins + setup environment for start dev

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,33 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+    app: {
+        head: {
+            titleTemplate: 'Paideia - %s',
+            htmlAttrs: {
+                lang: 'ru'
+            },
+            meta: [
+                { charset: 'utf-8' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                { hid: 'description', name: 'description', content: '' },
+            ],
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            ],
+        },
+        pageTransition: { name: 'page', mode: 'out-in' },
+        layoutTransition: { name: 'layout', mode: 'out-in' }
+    },
+
+    compatibilityDate: '2024-04-03',
+    devtools: {
+        enabled: true
+    },
+    css: [
+        '~/assets/scss/_bootstrap.scss',
+        '~/assets/scss/_index.scss',
+    ],
+    modules: [
+        '@pinia/nuxt',
+        '@bootstrap-vue-next/nuxt'
+    ],
 })
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+    useHead({
+        title: 'Главная',
+    })
+</script>
+
+<template>
+    <main class="container">
+        <div class="row">
+            <div class="col-6"><h1>Hello</h1></div>
+        </div>
+    </main>
+</template>
